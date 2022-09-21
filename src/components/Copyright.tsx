d5bb54c9f9ed8fcaf995ac8fbb638dd002ffb442
Jacob Sexton <jsextonprofessional@protonmail.com>
2022-09-21 12:32:34 -0500

    Added appropriate padding to Copyright component.

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,7 +1,7 @@
 export const Copyright = () => {
   return (
-    <div className="mt-12 bg-[#15469B] pt-8">
-      <p className="px-6 pb-8 text-sm text-white text-left">
+    <div className="bg-[#15469B] px-[16px] py-[8px]">
+      <p className="text-sm text-white text-left">
         &copy; Copyright Snap! Mobile Inc., 2014-2022, All Rights Reserved
       </p>
     </div>
