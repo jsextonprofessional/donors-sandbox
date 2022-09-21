5f2bbb3bb60e80ecf839a1bb3c26708d076745a8
Jacob Sexton <jsextonprofessional@protonmail.com>
2022-09-21 17:18:11 -0500

    Added margins to right side content. Added nowrap to address text.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,8 +10,8 @@ export const Footer = () => {
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
-      <div className="border-4 border-orange-500 flex flex-col sm:flex-row sm:justify-between px-[16px] py-[24px]">
-        <div className="flex flex-col border-4 border-red-500">
+      <div className="flex flex-col sm:flex-row sm:justify-between px-[16px] py-[24px]">
+        <div className="flex flex-col">
           <h4 className="font-semibold self-start text-[16px]">Support</h4>
           <div className="grid grid-cols-2 sm:grid-cols-1 my-2">
             <a href="" className="justify-self-start text-[14px]">
@@ -22,8 +22,8 @@ export const Footer = () => {
             </a>
           </div>
         </div>
-        <div className="border-4 border-purple-500 sm:flex sm:flex-row">
-          <div className="flex flex-col my-4 sm:my-0 border-4 border-yellow-300">
+        <div className="sm:flex sm:flex-row sm:mr-[40px]">
+          <div className="flex flex-col my-4 sm:mr-8 sm:my-0">
             <div className="font-medium italic mb-1 self-start text-[14px]">
               powered by...
             </div>
@@ -34,12 +34,12 @@ export const Footer = () => {
               <SnapLogo product="snap-logo-raise" white="true" variant="stacked" />
             </div>
           </div>
-          <div className="border-4 border-green-500 flex flex-col">
+          <div className="flex flex-col">
             <div className="flex flex-col self-start text-[14px]">
               <h5 className="self-start font-semibold">Snap! Mobile</h5>
-              <p>8300 7th Ave S Seattle, WA 98108</p>
+              <p className="whitespace-nowrap">8300 7th Ave S Seattle, WA 98108</p>
             </div>
-            <p className="my-1 self-start text-[14px]">support@snap-raise.com</p>
+            <p className="my-1 sm:my-2 self-start text-[14px]">support@snap-raise.com</p>
             <p className="self-start text-[14px]">206.285.0906</p>
           </div>
         </div>
